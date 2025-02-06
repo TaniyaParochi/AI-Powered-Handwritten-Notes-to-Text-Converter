0985be5a4c0cc9970d8549b3b99fa24b230ae130
TaniyaParochi <163005378+TaniyaParochi@users.noreply.github.com>
2025-02-06 22:17:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Railway from GitHub!"
+    return "Hello, This is Taniya Project!"
 
 if __name__ == "__main__":
     app.run(debug=True)
